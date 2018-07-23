ede3217da038747bb52d78ce531caef4f17d193c
Martin Filo <martinxfilo@gmail.com>
2018-07-23 13:00:43 +0200

    is folder

--- src/app/tree.mock.ts
+++ src/app/tree.mock.ts
@@ -6,30 +6,35 @@ export const TREE: INode[] = [
     id: 'root',
     pathToNode: '',
     parentId: 'root',
+    isFolder: true,
     childrenIds: ['folder1', 'folder2']
   }, <INode>{
     name: 'folder1',
     id: 'folder1',
     pathToNode: '/folder1',
     parentId: '',
+    isFolder: true,
     childrenIds: ['uniqueId4']
   }, <INode>{
     name: 'folder2',
     id: 'folder2',
     pathToNode: '/folder2',
     parentId: '',
+    isFolder: true,
     childrenIds: ['uniqueId3']
   }, <INode> {
     name: 'folder3',
     id: 'folder3',
     pathToNode: '/folder2/folder3',
     parentId: 'folder2',
+    isFolder: true,
     childrenIds: []
   }, <INode> {
-    name: 'folder4',
-    id: 'folder4',
-    pathToNode: '/folder1/folder4',
+    name: 'file1.bla',
+    id: 'file1.bla',
+    pathToNode: '/folder1/file1.bla',
     parentId: 'folder1',
+    isFolder: false,
     childrenIds: []
   }
 ];
--- src/app/tree/interfaces/i-node.ts
+++ src/app/tree/interfaces/i-node.ts
@@ -2,6 +2,7 @@ export interface INode {
   id: string;
   parentId: string;
   pathToNode: string;
+  isFolder: boolean;
   name?: string;
   childrenIds?: string[];
 }
