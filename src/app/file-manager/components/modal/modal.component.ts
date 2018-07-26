01518a41f7d3e7915db1f39afafff2db847832ec
Martin Filo <martinxfilo@gmail.com>
2018-07-26 13:22:18 +0200

    modal loading too long

--- src/app/file-manager/components/modal/modal.component.ts
+++ src/app/file-manager/components/modal/modal.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AppStore} from '../../reducers/reducer.factory';
 import {SET_LOADING_STATE} from '../../reducers/actions.action';
+import {Observable, timer} from 'rxjs';
 
 @Component({
   selector: 'app-modal',
@@ -9,11 +10,15 @@ import {SET_LOADING_STATE} from '../../reducers/actions.action';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  problem: string;
 
   constructor(private store: Store<AppStore>) {
   }
 
   ngOnInit() {
+    timer(2000).subscribe(() => {
+      this.problem = 'Troubles with loading? Klik sem na zrusenie nacitavania.';
+    });
   }
 
   clicked() {
