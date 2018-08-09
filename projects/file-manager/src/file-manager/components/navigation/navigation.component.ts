8791cdca41d5547d1eefd40ae72578b25309232f
Martin Filo <martinxfilo@gmail.com>
2018-08-09 14:06:51 +0200

    added search bar to navigation component

--- projects/file-manager/src/file-manager/components/navigation/navigation.component.ts
+++ projects/file-manager/src/file-manager/components/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavigationComponent implements OnInit {
 
