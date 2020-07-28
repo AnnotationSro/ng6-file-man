a08780eb29460b8458478f1b5917206b32b5c480
Martin Filo <martinxfilo@gmail.com>
2020-07-28 15:08:41 +0200

    2.2.1 - fix public_api.ts

--- projects/file-manager/src/public_api.ts
+++ projects/file-manager/src/public_api.ts
@@ -7,4 +7,5 @@ export * from './file-manager/file-manager.module';
 export * from './file-manager/models/tree.model';
 export * from './file-manager/interfaces/config.interface';
 export * from './file-manager/interfaces/node.interface';
+export * from './file-manager/enums/download-mode.enum';
 export * from './file-manager/services/node.service';
